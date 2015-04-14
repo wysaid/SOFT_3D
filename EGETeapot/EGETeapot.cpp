7f62fb2024256794437d64b9cf46b205d6e3c720
wangyang <admin@wysaid.org>
2015-04-14 17:26:02 +0800

    完善茶壶部分

--- EGETeapot/EGETeapot.cpp
+++ EGETeapot/EGETeapot.cpp
@@ -1,6 +1,8 @@
 //Author: wysaid
 //blog: http://blog.wysaid.org
 
+#define SHOW_CONSOLE
+
 #include "cgeMat.h"
 #include "cgeVec.h"
 #include "graphics.h"
@@ -9,17 +11,195 @@
 
 using namespace CGE;
 
+#define USE_DEPTH_TEST 0
+#define USE_PERSPECTIVE_PROJ 1
+
+#define RENDER_TRIANGLE_HALF_1 1
+#define RENDER_TRIANGLE_HALF_2 0
+
+color_t g_color;
+
+
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 600
+
+#if USE_DEPTH_TEST
+//depth 还存在一点问题， 没有效果
+unsigned char g_depthMask[SCREEN_WIDTH][SCREEN_HEIGHT];
+const int g_maskSize = sizeof(g_depthMask);
+
+#endif
+
+template<class Type>
+class Triangle
+{
+public:
+
+	static inline void fillTriangle(const Type& v0, const Type& v1, const Type& v2)
+	{
+		if(v0[1] == v2[1])
+		{
+			_fillSimpleTriangle(v0, v1, v2);
+		}
+		else if(v1[1] == v2[1])
+		{
+			_fillSimpleTriangle(v1, v0, v2);
+		}
+		else if(v0[1] == v1[1])
+		{
+			_fillSimpleTriangle(v0, v2, v1);
+		}
+		else
+		{
+			_fillNormalTriangle(v0, v1, v2);
+		}
+	}
+
+private:
+
+#if USE_DEPTH_TEST
+
+	static inline void drawPixel(int x, int y, int depth, color_t color)
+	{
+		if(x < 0 || x >= SCREEN_WIDTH || y < 0 || y >= SCREEN_HEIGHT)
+			return;
+
+		if(depth >=0 && depth < 256 && g_depthMask[x][y] <= depth)
+		{
+			putpixel_f(x, y, color);
+			g_depthMask[x][y] = depth;
+		}
+	}
+
+	static inline void drawLineL2R(int x0, int depth0, int x1, int depth1, int y, color_t color)
+	{
+		if(x0 == x1)
+		{
+			drawPixel(x0, y, depth0, color);
+			return;
+		}
+
+		int left, right;
+		int depthL, depthR;
+
+		if(x0 < x1)
+		{
+			left = x0;
+			right = x1;
+			depthL = depth0;
+			depthR = depth1;
+		}
+		else
+		{
+			left = x1;
+			right = x0;
+			depthL = depth1;
+			depthR = depth0;
+		}
+
+		float delta = float(depthR - depthL) / (right - left);
+		for(int i = left; i <= right; ++i)
+		{
+			drawPixel(i, y, depthL, color);
+			depthL += delta;
+		}
+	}
+
+#endif
+
+	// 平顶/平底三角形, v0[1] == v2[1], v1[1] 最大
+	static inline void _fillSimpleTriangle(const Type& v0, const Type& v1, const Type& v2)
+	{
+		assert(v0[1] == v2[1]);
+		float h = v1[1] - v0[1];
+
+		float dL = (v1[0] - v0[0]) / h;
+		float dR = (v1[0] - v2[0]) / h;
+
+		float dDepthL = (v1[2] - v0[2]) / h;
+		float dDepthR = (v1[2] - v2[2]) / h;
+
+		float xL = v0[0], xR = v2[0];
+		float depthL = v0[2], depthR = v2[2];
+
+		if(v0[1] < v1[1])
+		{
+#if RENDER_TRIANGLE_HALF_1
+
+			for(int i = v0[1]; i <= v1[1]; ++i)
+			{
+#if USE_DEPTH_TEST
+				drawLineL2R(xL, depthL, xR, depthR, i, RED);
+#else
+				line(xL, i, xR, i); //A Simple Function That Just Draw A Line
+#endif //USE_DEPTH_TEST
+
+				xL += dL;
+				xR += dR;
+
+				depthL += dDepthL;
+				depthR += dDepthR;
+			}
+#endif //RENDER_TRIANGLE_HALF_1
+		}
+		else
+		{
+#if RENDER_TRIANGLE_HALF_2
+
+			for(int i = v0[1]; i >= v1[1]; --i)
+			{
+#if USE_DEPTH_TEST
+				drawLineL2R(xL, depthL, xR, depthR, i, YELLOW);
+#else
+				line(xL, i, xR, i); //A Simple Function That Just Draw A Line
+#endif //USE_DEPTH_TEST	
+
+				xL -= dL;
+				xR -= dR;
+
+				depthL -= dDepthL;
+				depthR -= dDepthR;
+			}
+
+#endif
+		}
+	}
+
+
+	static inline void _fillNormalTriangle(const Type& v0, const Type& v1, const Type& v2)
+	{
+		const Type *pnts[] = {&v0, &v1, &v2};
+
+		if((*pnts[0])[1] > (*pnts[1])[1])
+			std::swap(pnts[0], pnts[1]);
+
+		if((*pnts[0])[1] > (*pnts[2])[1])
+			std::swap(pnts[0], pnts[2]);
+
+		if((*pnts[1])[1] > (*pnts[2])[1])
+			std::swap(pnts[1], pnts[2]);		
+
+		const Type &vv0 = *pnts[0], &vv1 = *pnts[1], &vv2 = *pnts[2];
+		Type newPoint(vv1);
+
+		newPoint[0] = vv0[0] + (vv2[0] - vv0[0]) * (vv1[1] - vv0[1]) / (vv2[1] - vv0[1]);
+
+		_fillSimpleTriangle(newPoint, vv0, vv1);
+		_fillSimpleTriangle(newPoint, vv2, vv1);
+	}
+};
+
 class Object
 {
 public:
 	Object()
 	{
 		//把下面的0(1)换成1(0)可以切换两种视图
-#if 1
- 		m_matProj = Mat4::makePerspective(M_PI / 4.0f, 4.0f / 3.0f, 1.0, 1000.0f);
- 		m_matModelView = Mat4::makeLookAt(0.0f, 0.0f, 800.0f, 0.0f, 0.0f, -1000.0f, 0.0f, 1.0f, 0.0f);
+#if USE_PERSPECTIVE_PROJ
+ 		m_matProj = Mat4::makePerspective(M_PI / 4.0f, 4.0f / 3.0f, 1.0f, 1000.0f);
+ 		m_matModelView = Mat4::makeLookAt(0.0f, 0.0f, 800.0f, 0.0f, 0.0f, -500.0f, 0.0f, 1.0f, 0.0f);
 #else
- 		m_matProj = Mat4::makeOrtho(-400.0f, 400.0f, -300.0f, 300.0f, -1.0f, 1.0f);
+ 		m_matProj = Mat4::makeOrtho(-400.0f, 400.0f, -300.0f, 300.0f, -300.0f, 300.0f);
  		m_matModelView.loadIdentity();
 #endif
 	}
@@ -27,12 +207,14 @@ public:
 
 	void render(float x, float y)
 	{
-		std::vector<Vec2f> vec;
-		Mat4 mvp = m_matProj * m_matModelView;
+		std::vector<Vec3f> vec;
+		const Mat4 mvp = m_matProj * m_matModelView;
+		const Vec4f viewPort(0.0f, 0.0f, 800.0f, 600.0f);
 		for(auto t = m_vec.begin(); t != m_vec.end(); ++t)
 		{
-			Vec2f coord;
-			Mat4::projectM4f(*t, mvp, coord, 800.0f, 600.0f);
+			Vec3f coord;
+			Mat4::projectM4f(*t, mvp, viewPort, coord);
+			//Mat4::projectM4fPerspective(*t, m_matModelView, m_matProj, viewPort, coord);
 			vec.push_back(coord);
 		}
 
@@ -41,8 +223,38 @@ public:
 			const int index1 = g_teapotIndices[i];
 			const int index2 = g_teapotIndices[i + 1];
 			const int index3 = g_teapotIndices[i + 2];
-			line(x + vec[index1][0], y + vec[index1][1], x + vec[index2][0], y + vec[index2][1]);
-			line(x + vec[index3][0], y + vec[index3][1], x + vec[index2][0], y + vec[index2][1]);
+			
+			const Vec3i polyPoints[] = {
+				Vec3i(x + vec[index1][0], y + vec[index1][1], vec[index1][2] * 255.0f),
+				Vec3i(x + vec[index2][0], y + vec[index2][1], vec[index2][2] * 255.0f),
+				Vec3i(x + vec[index3][0], y + vec[index3][1], vec[index3][2] * 255.0f) };
+
+//设置是否显示模拟填充
+#if RENDER_TRIANGLE_HALF_1 || RENDER_TRIANGLE_HALF_2
+			
+			{
+				float ax = polyPoints[1][0] - polyPoints[0][0];
+				float ay = polyPoints[1][1] - polyPoints[0][1];
+				float bx = polyPoints[2][0] - polyPoints[1][0];
+				float by = polyPoints[2][1] - polyPoints[1][1];
+				float zNorm = ax * by - ay * bx;
+
+				//背面剔除
+				if(zNorm > 0.0f)
+				{
+					setcolor(g_color ^ 0xffffff);
+#if USE_DEPTH_TEST
+					memset(g_depthMask, 0, g_maskSize);
+#endif
+					Triangle<Vec3i>::fillTriangle(polyPoints[0], polyPoints[1], polyPoints[2]);
+				}
+			}
+
+#endif
+			setcolor(g_color);
+			line(polyPoints[0][0], polyPoints[0][1], polyPoints[1][0], polyPoints[1][1]);
+			line(polyPoints[1][0], polyPoints[1][1], polyPoints[2][0], polyPoints[2][1]);
+			line(polyPoints[2][0], polyPoints[2][1], polyPoints[0][0], polyPoints[0][1]);
 		}
 
 	}
@@ -143,10 +355,19 @@ void getObj(Object& obj, float scale)
 	}
 }
 
+// template<class Type>
+// void drawTriangle(const Type& t0, const Type& t1, const Type& t2)
+// {
+// 	line(t0[0], t0[1], t1[0], t1[1]);
+// 	line(t2[0], t2[1], t1[0], t1[1]);
+// 	line(t0[0], t0[1], t2[0], t2[1]);
+// }
+
 int main()
 {
-	initgraph(800, 600);
+	initgraph(SCREEN_WIDTH, SCREEN_HEIGHT);
 	setrendermode(RENDER_MANUAL);
+
 	Object obj;
 	randomize();
 	
@@ -158,6 +379,7 @@ int main()
 	float dx = randomf()* 5.0f + 1.0f, dy = randomf()* 5.0f + 1.0f;
 	float radX = randomf()*10.0f + 0.001f, radY = randomf() * 10.0f, radZ = randomf() * 10.0f;
 	setbkmode(TRANSPARENT);
+	setfillcolor(DARKGRAY);
 	for(; is_run(); delay_fps(60))
 	{
 		if(s_useBlur) imagefilter_blurring(0, 0x80, 0x100);
@@ -165,9 +387,9 @@ int main()
 		mouseFunc(obj);
 
 		obj.render(0, 0);
-		outtextxy(20, 10, "点击空格键启用模糊滤镜,按回车随机生成模型，滚轮移动模型Z值");
-		
-		setcolor(HSVtoRGB(i*2, 1.0f, 1.0f));
+
+		g_color = HSVtoRGB(i*2, 1.0f, 1.0f);
+		outtextxy(20, 10, "点击空格键启用模糊滤镜, 滚轮移动模型Z值");
 
 		x += dx;
 		y += dy;
